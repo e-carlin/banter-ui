6dfafe3f46f50d4ea05abf087951019721d208da
Evan Carlin <evan@carlin.com>
2017-12-12 22:36:00 -0800

    Better user registration error handling

--- src/actions/userRegistration.js
+++ src/actions/userRegistration.js
@@ -33,28 +33,66 @@ export function registerUser(email, password) {
     console.log("pass is "+password)
     return (dispatch) => {
         dispatch(userRegistrationIsPending(true));
-
         registerUserApi(email, password)
-            .then((response) => {
-                dispatch(userRegistrationIsPending(false));
-                if (!response.ok) {
-                    dispatch(userRegistrationSuccess(false));
-                     if(response.status === 400 || response.status === 409){ //400 code is an error likely means there is an internal status code that should be proccessed
-                        response.json().then((res) => {
-                            dispatch(userRegistrationHasErrored(res.message))
-                        })
-                    }
-                    else {
-                        dispatch(userRegistrationHasErrored("Sorry, there was an internal error. Please try again."));
-                    }
-                    //Throw this error so the catch is triggered and no further processing of the response is done
-                    throw Error(response.statusText)
-                }
+        .then((response) => {
+            dispatch(userRegistrationIsPending(false));
+            if(response.ok) {
+                dispatch(userRegistrationSuccess(true));
+                return;
+            }
+            else {
+                throw response;
+            }
+        })
+        .catch((response) => {
+            dispatch(userRegistrationIsPending(false))
+            if(response.status === 400 || response.status === 409){ //400 code is an error likely means there is an internal status code that should be proccessed
+                response.json().then((res) => {
+                dispatch(userRegistrationHasErrored(res.message))
+                dispatch(userRegistrationSuccess(false))
+                })
+            }
+            else {
+                dispatch(userRegistrationHasErrored("There was an error registering. Please try again."))
+                //TODO: More in depth error handling
+                console.log("ERROR: "+response)
+            }
+        })
+        }
+    }
 
-                return response;
-            })
-            .then((response) => response.json())
-            .then(() => dispatch(userRegistrationSuccess(true)))
-            .catch((e) => console.log("Error registaring user: "+e));
-    };
-}
+// export function registerUser(email, password) {
+//     console.log("email is "+email)
+//     console.log("pass is "+password)
+//     return (dispatch) => {
+//         dispatch(userRegistrationIsPending(true));
+
+//         registerUserApi(email, password)
+//             .then((response) => {
+//                 dispatch(userRegistrationIsPending(false));
+//                 if (!response.ok) {
+//                     dispatch(userRegistrationSuccess(false));
+//                      if(response.status === 400 || response.status === 409){ //400 code is an error likely means there is an internal status code that should be proccessed
+//                         response.json().then((res) => {
+//                             dispatch(userRegistrationHasErrored(res.message))
+//                         })
+//                     }
+//                     else {
+//                         dispatch(userRegistrationHasErrored("Sorry, there was an internal error. Please try again."));
+//                     }
+//                     //Throw this error so the catch is triggered and no further processing of the response is done
+//                     throw Error(response.statusText)
+//                 }
+
+//                 return response;
+//             })
+//             // .catch((e) => console.log("Error in the request"))
+//             .then((response) => response.json())
+//             .then(() => dispatch(userRegistrationSuccess(true)))
+//             .catch((e) => {
+//                 dispatch(userRegistrationIsPending(false));
+//                 dispatch(userRegistrationHasErrored("In catch"))
+//                 console.log("Error registaring user: "+e);
+//             })
+//     };
+// }
